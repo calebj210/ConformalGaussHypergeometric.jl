fbc86aa440b4c03a8c8aeac8af1a0500a757a840
Caleb Jacobs <calebjacobs210@gmail.com>
2025-12-11 09:00:32 -0700

    (version) changed to release

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,11 +11,6 @@ using Test
         @test conformal2f1(a,b,c,cispi(1/3)) ≈ (0.4965103388730277 + 0.873467605231226im)      rtol=1e-14
     end
 
-    @testset "Integer Parameters" begin
-        @test conformal2f1(1.1,1.2,1.3,1.5+1.5im) ≈ (-0.22147498072051158 + 0.5964515057937457im) rtol=1e-14 skip=true
-        @test conformal2f1(-2,-3,-4,.5) ≈ () rtol=1e-14 skip=true
-    end
-    
     @testset "Error Estimation" begin
         (a,b,c) = (1.1,1.22,1.333)
         @test conformal2f1(a,b,c,.5; esterr=true)[2] < 1e-13
